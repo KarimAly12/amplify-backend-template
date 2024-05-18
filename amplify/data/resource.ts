e69b489aab80f24528b3f00ae87b5944f2229d55
KarimAly <88921421+KarimAly12@users.noreply.github.com>
2024-05-19 00:56:43 +1000

    .

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -9,7 +9,7 @@ specifies that any user authenticated via an API key can "create", "read",
 const schema = a.schema({
   Child: a
     .model({
-      id: a.string(),
+      childID: a.string(),
       firstName: a.string(),
       lastName: a.string(),
       inTrip: a.boolean()
